d184c3c585aa8b3e9b79e11f90bc911303f18551
binayakadhikari <blazebnayak@gmail.com>
2022-09-08 20:22:02 +0545

    added keyboard routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const transactionRouter = require('./server/routes/transactionRoutes');
 const authRouter = require('./server/routes/authenticationRoutes');
 const bankRouter = require('./server/routes/bankRoutes');
+const keywordRouter = require('./server/routes/keywordRoutes');
 const appError = require('./server/utils/appError');
 
 const globalErrorHandler = require('./server/controller/errorController');
@@ -16,6 +17,7 @@ app.use(morgan('dev'));
 app.use("/api/v1/transactions", transactionRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/banks", bankRouter);
+app.use("/api/v1/keywords", keywordRouter);
 
 app.all('*', (req, res, next) => {
   next(new appError(`Can't find ${req.originalUrl} on this server!`, 404));
